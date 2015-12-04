e9f8f6124e641c9cd49f2c6b1594c17f66110b8c
emilio <emilio.dorigatti@gmail.com>
2015-12-04 12:12:05 +0100

    generating some random noise based on current day

--- pyspider/scheduler/dynamic_token_bucket.py
+++ pyspider/scheduler/dynamic_token_bucket.py
@@ -12,30 +12,46 @@ class DynamicBucket(Bucket):
     distribution obtained by summing two independent normal random variables.
     If burst is positive then the standart token-bucket algorithm is used.
     """
+
+    # amplitude is further scaled based on weekdays
+    weekday_scaling = [1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.3]
+
     def __init__(self, rate=1, burst=None, timezone=None,
                  dist1=(10, 2, 1),     # mu, sigma, scaling
                  dist2=(16, 2, 0.7)):  # mu, sigma, scaling
         super(DynamicBucket, self).__init__(rate, burst)
         self.timezone = timezone or pytz.timezone(random.choice(pytz.common_timezones))
 
-        xs = [x / 60. for x in range(0, 24 * 60)]
-        self.rate_time = map(sum, zip(self.compute_distribution(xs, *dist1),
-                                      self.compute_distribution(xs, *dist2)))
-        self.ymax = max(self.rate_time)
         self._real_rate = rate
         self._real_burst = burst
+        self.dist1, self.dist2 = dist1, dist2
 
-    def compute_distribution(self, xs, mu, sigma, scale):
-        def pdf(x):
-            return math.exp(-(x-mu)**2/(2*sigma**2))/math.sqrt(2*math.pi*sigma)
-        return [pdf(x) * scale for x in xs]
+    def daily_random_noise(self, now, x, noise_mu=0, noise_sigma=0.1):
+        """ Alter x by a small, random, normally-distributed amount (the same each day)
+        """
+        old = random.getstate()
+        random.seed(now.year * 365 + now.month * 31 + now.day)
+        val = x + random.normalvariate(noise_mu, noise_sigma)
+        random.setstate(old)
+        return val
+
+    def probability_density_function(self, x, mu, sigma, scale):
+        now = datetime.now(self.timezone)
+        mu = self.daily_random_noise(now, mu, 0, mu * 0.1)
+        sigma = max(0.1, self.daily_random_noise(now, sigma, 0, sigma * 0.1))
+        normal = math.exp(-(x-mu)**2/(2*sigma**2))/math.sqrt(2*math.pi*sigma)
+        return scale * normal * self.weekday_scaling[now.weekday()]
 
     @property
     def rate(self):
         if self._real_burst < 0.0:
             now = datetime.now(self.timezone)
-            x = now.hour * 60 + now.minute
-            return self.rate_time[x] * self._real_rate / self.ymax
+            x = now.hour + now.minute / 60.
+            y1 = self.probability_density_function(x, *self.dist1)
+            y2 = self.probability_density_function(x, *self.dist2)
+            ymax = max(self.dist1[2] / math.sqrt(2 * math.pi),
+                       self.dist2[2] / math.sqrt(2 * math.pi))
+            return (y1 + y2) * self._real_rate / ymax
         else:
             return self._real_rate
 
